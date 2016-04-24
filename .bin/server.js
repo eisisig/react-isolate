ceabb715ab3633f1f37a1c664fa9506c9433cbad
Eisi Sig <eisi@meniga.is>
2016-04-24 22:41:33 +0000

    Chaning the ascii image on start

--- .bin/server.js
+++ .bin/server.js
@@ -1,27 +1,25 @@
-const WebpackDevServer = require('webpack-dev-server');
-const webpack = require('webpack');
-const path = require("path");
-const pkg = require("../package.json");
+const WebpackDevServer = require('webpack-dev-server')
+const webpack = require('webpack')
+const path = require("path")
+const style = require('ansi-styles')
+const pkg = require("../package.json")
 
-// const isolateConfig = require('../isolate.config');
-const webpackConfig = require('../webpack.config');
-
-webpackConfig.entry.unshift(
-	'webpack-dev-server/client',
-	'webpack/hot/dev-server'
-);
-
-webpackConfig.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+// const isolateConfig = require('../isolate.config')
+const webpackConfig = require('../webpack.config')
 
 var banner = function () {
-	console.log(`
-		                                         _
-	  ,_   _  __,   __  -/-     .  ,    _,_ // __,  -/- _
-	_/ (__(/_(_/(__(_,__/_    _/__/_)__(_/_(/_(_/(__/__(/_     v${pkg.version} ...starting
-`);
-};
+	console.log(`${style.blue.open}
+                    |       o          |         |         
+,---.,---.,---.,---.|---    .,---.,---.|    ,---.|--- ,---.
+|    |---',---||    |    ---|\`---.|   ||    ,---||    |---'
+\`    \`---'\`---^\`---'\`---'   \`\`---'\`---'\`---'\`---^\`---'\`---'
 
-const compiler = webpack(webpackConfig);
+v${pkg.version} ...starting
+${style.blue.close}
+`)
+}
+
+const compiler = webpack(webpackConfig)
 
 const server = new WebpackDevServer(compiler, {
 	contentBase: path.resolve(__dirname, '..', 'isolate-src'),
@@ -38,6 +36,6 @@ const server = new WebpackDevServer(compiler, {
 		hash: false,
 		chunkModules: false
 	}
-});
+})
 
-server.listen(9999, () => banner());
+server.listen(9999, () => banner())
